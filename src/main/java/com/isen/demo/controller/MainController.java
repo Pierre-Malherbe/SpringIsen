dc77ed54a234b80646de71a038976e57fae1e1d6
Pierre Malherbe <pierre.malherbe6@gmail.com>
2019-02-10 16:17:16 +0100

    First commit

---
+++ src/main/java/com/isen/demo/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.isen.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String Index() {
+        return "Isen Works ! ";
+    }
+}
